09768cde45c5452ec409def865f59554e674de51
meyravDayan <48184955+meyravDayan@users.noreply.github.com>
2019-03-10 19:50:06 +0200

    Create hw1_question1.py

---
+++ hw1_question1.py
@@ -0,0 +1,22 @@
+def trifeca(word):
+    """
+    Checks whether word contains three consecutive double-letter pairs.
+    word: string
+    returns: bool
+    """
+    counter = 0
+    if len(word) < 6:
+        return False
+    else:
+        len_word = len(word)-5
+        for ii in range(0,len_word):
+            if (word[ii] == word[ii+1]) and (word[ii+2] == word[ii+3]) and (word[ii+4] == word[ii+5]) and word[ii:ii+6].isalpha()==True:
+                counter += 1
+            else:
+                counter = counter
+    if counter > 0:
+        return True
+    else:
+        return False
+            
+print(trifeca(word = '1122aabbcc4'))
